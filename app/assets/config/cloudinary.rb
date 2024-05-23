e89b593b6e11db501c5e6103fdf5cb77ff48fe19
Serban Ciurea <ciureaserban777@icloud.com>
2024-05-23 12:26:12 +0100

    changes to colour and layout

---
+++ app/assets/config/cloudinary.rb
@@ -0,0 +1,6 @@
+# Cloudinary.config do |config|
+#   config.cloud_name = 'dq1b1uqzm'
+#   config.api_key = '433754115748951'
+#   config.api_secret = 'Q-URJRg_XlVb6SFiaAqZcyoNWb0'
+#   config.secure = true
+# end
