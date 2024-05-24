aafe8b380d648ab84ae3112edf134e2a7a7eb2ac
Miriam Ciurea <miriamciurea777@icloud.com>
2024-05-24 20:07:02 +0300

    mama lor de poze...pai asa se face??

--- public/scroll.js
+++ public/scroll.js
@@ -30,3 +30,19 @@ document.body.onpointermove = event => {
 }
 
 // 3D image gallery from portofolio, home page
+
+let prev = document.querySelector(".btn-prev");
+let next = document.querySelector(".btn-next");
+let box = document.querySelector(".box");
+
+let degrees = 0;
+
+prev.addEventListener('click', function() {
+  degrees += 45;
+  box.style = `transform: perspective(1000px) rotateY(${degrees}deg);`;
+})
+
+next.addEventListener('click', function() {
+  degrees -= 45;
+  box.style = `transform: perspective(1000px) rotateY(${degrees}deg);`;
+})
