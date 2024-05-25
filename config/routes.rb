a12f68c4cc4a78cee08e49f81405ff716c58c3b4
Miriam Ciurea <miriamciurea777@icloud.com>
2024-05-25 17:48:51 +0300

    nu esti pregatit de asa ceva

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :project_ports
   resources :jobs
+  get 'contacts/new', to: 'contacts#new', as: 'new_contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
