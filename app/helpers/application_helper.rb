2bd783f1b61d05550f77a80071e24e69668cc168
Miriam Ciurea <miriamciurea777@icloud.com>
2024-06-21 16:16:29 +0300

    quill.js integrated (idk how, but I even debugged it)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def format_description(description)
+    formatted_description = description.dup
+
+    # Handle bold formatting: *text* to <strong>text</strong>
+    formatted_description.gsub!(/\*(.*?)\*/, '<strong>\1</strong>')
+
+    # Handle underline formatting: _text_ to <u>text</u>
+    formatted_description.gsub!(/_(.*?)_/, '<u>\1</u>')
+
+    # Convert line breaks to <br> tags
+    formatted_description.gsub!(/\r\n/, '<br>')
+
+    formatted_description.html_safe
+  end
+
 end
