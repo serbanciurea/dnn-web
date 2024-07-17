3222d0778af5f26362382baf09a4b9e2dfa566ff
Miriam Ciurea <miriamciurea777@icloud.com>
2024-07-17 14:40:31 +0300

    user profile ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_081145) do
-# ActiveRecord::Schema[7.1].define(version: 2024_07_16_124809) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_16_124809) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
