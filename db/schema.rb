7d8218f7d2d5136e5280dc1004e858fc41e14f47
Serban Ciurea <ciureaserban777@icloud.com>
2024-07-19 10:22:19 +0300

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_17_135234) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_18_131603) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -94,6 +94,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_135234) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
+    t.float "latitude"
+    t.float "longitude"
     t.string "address"
     t.string "sponsor"
     t.boolean "driver", default: false
