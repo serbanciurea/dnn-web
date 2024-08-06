12cf56a06139dbb461d6ef314d4c32692c00568f
Miriam Ciurea <miriamciurea777@icloud.com>
2024-08-06 18:22:10 +0300

    approved to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_113055) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_152114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -101,6 +101,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_113055) do
     t.boolean "driver", default: false
     t.string "pts_number"
     t.boolean "admin"
+    t.boolean "approved"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
