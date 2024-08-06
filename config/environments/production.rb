28d2b2aa75a06a64a5145950ceacbf55efc43b55
Miriam Ciurea <miriamciurea777@icloud.com>
2024-08-06 16:06:28 +0300

    mama

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -136,6 +136,11 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.digest = true
+
+  config.assets.version = '1.0'
+
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server (optional).
   # config.asset_host = "http://assets.example.com"
 
