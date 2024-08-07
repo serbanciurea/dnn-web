2ae687f364b587f5f03dafbef9c39da14c3f08a3
Miriam Ciurea <miriamciurea777@icloud.com>
2024-08-07 18:33:27 +0300

    .

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,24 +1,24 @@
 class Job < ApplicationRecord
   # belongs_to :user
-  validates :title, :description, :location, :salary, presence: true
-  geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
-  before_save :geocode, if: :will_save_change_to_location?
-  after_validation :geocode, if: :will_save_change_to_location?
-  after_save :geocode_location, if: :location_changed?
+  validates :title, :location, :salary, presence: true
+  # geocoded_by :location
+  # after_validation :geocode, if: :will_save_change_to_location?
+  # before_save :geocode, if: :will_save_change_to_location?
+  # after_validation :geocode, if: :will_save_change_to_location?
+  # after_save :geocode_location, if: :location_changed?
 
-  private
+  # private
 
-  def geocode_location
-    return if location.blank?
+  # def geocode_location
+  #   return if location.blank?
 
-    results = Geocoder.search(location)
-    if results.present?
-      self.update(latitude: results.first.latitude, longitude: results.first.longitude)
-    else
-      Rails.logger.error "Geocoding failed for location #{location}"
-    end
-  rescue StandardError => e
-    Rails.logger.error "Geocoding error: #{e.message}"
-  end
+  #   results = Geocoder.search(location)
+  #   if results.present?
+  #     self.update(latitude: results.first.latitude, longitude: results.first.longitude)
+  #   else
+  #     Rails.logger.error "Geocoding failed for location #{location}"
+  #   end
+  # rescue StandardError => e
+  #   Rails.logger.error "Geocoding error: #{e.message}"
+  # end
 end
