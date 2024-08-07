deaf3c7ee3ac7051f9dba7ac8487f2d71dcdbfb4
Miriam Ciurea <miriamciurea777@icloud.com>
2024-08-07 18:02:24 +0300

    .

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,6 @@
 class ContactsController < ApplicationController
 
   skip_before_action :authenticate_user!
-  skip_before_action :authenticate_contact, only: [:new, :create]
   def new
     @contact = Contact.new
   end
