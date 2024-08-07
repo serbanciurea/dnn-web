413f279a15d0c8f36abdfbfb59877c371916ce3d
Miriam Ciurea <miriamciurea777@icloud.com>
2024-08-07 18:11:51 +0300

    .

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,22 @@ class Job < ApplicationRecord
   validates :title, :description, :location, :salary, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  before_save :geocode, if: :will_save_change_to_location?
+  after_validation :geocode, if: :will_save_change_to_location?
+  after_save :geocode_location, if: :location_changed?
+
+  private
+
+  def geocode_location
+    return if location.blank?
+
+    results = Geocoder.search(location)
+    if results.present?
+      self.update(latitude: results.first.latitude, longitude: results.first.longitude)
+    else
+      Rails.logger.error "Geocoding failed for location #{location}"
+    end
+  rescue StandardError => e
+    Rails.logger.error "Geocoding error: #{e.message}"
+  end
 end
