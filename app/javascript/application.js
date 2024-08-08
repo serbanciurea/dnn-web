1a0a47adf765d9d0c641c56313cbf6cb83dc7b19
Miriam Ciurea <miriamciurea777@icloud.com>
2024-08-08 10:13:32 +0300

    quill back baby

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "stylesheets/components/home/aboutus"
-import "./read_more.js"
+// import "@hotwired/turbo-rails"
+// import "controllers"
+// import "stylesheets/components/home/aboutus"
+// import "./read_more.js"
 // = require quill.global
