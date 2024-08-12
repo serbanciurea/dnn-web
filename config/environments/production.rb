da810cc0f665ed0f5d5567eab7142a8f92ff8b88
Miriam Ciurea <miriamciurea777@icloud.com>
2024-08-12 10:55:10 +0300

    jk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -134,7 +134,7 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.assets.digest = true
 
